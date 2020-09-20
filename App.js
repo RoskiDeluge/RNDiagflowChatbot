347a4c9f262897bf5eccfcfa9980cc4db8d9f53c
Roberto Delgado <roberto.delgado@gmail.com>
2020-09-20 00:55:29 -1000

    Changed dotenv to react-native-dotenv

--- App.js
+++ App.js
@@ -2,24 +2,23 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
-import { Dialogflow_V2 } from 'react-native-dialogflow';
-import { dialogflowConfig } from './env';
+import {Dialogflow_V2} from 'react-native-dialogflow';
+import {dialogflowConfig} from './env';
+
+const BOT_USER = {
+  _id: 2,
+  name: 'FAQ Bot',
+  avatar: 'https://i.imgur.com/7k12EPD.png',
+};
 
 class App extends Component {
-  
-  const BOT_USER = {
-    _id: 2,
-    name: 'FAQ Bot',
-    avatar: 'https://i.imgur.com/7k12EPD.png',
-  };
-  
   state = {
     messages: [
       {
         _id: 1,
         text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you today?`,
         createdAt: new Date(),
-        user: BOT_USER
+        user: BOT_USER,
       },
     ],
   };
@@ -29,7 +28,7 @@ class App extends Component {
       dialogflowConfig.client_email,
       dialogflowConfig.private_key,
       Dialogflow_V2.LANG_ENGLISH_US,
-      dialogflowConfig.project_id
+      dialogflowConfig.project_id,
     );
   }
 
@@ -41,9 +40,27 @@ class App extends Component {
     let message = messages[0].text;
     Dialogflow_V2.requestQuery(
       message,
-      result => this.handleGoogleResponse(result),
-      error => console.log(error)
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error),
     );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER,
+    };
+
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
   }
 
   render() {
--- env.js
+++ env.js
@@ -1,8 +1,10 @@
+import {FAQ_KEY_ID, FAQ_KEY} from '@env';
+
 export const dialogueFlow = {
   type: 'service_account',
   project_id: 'rndiagbot',
-  private_key_id: process.env.FAQ_KEY_ID,
-  private_key: process.env.FAQ_KEY,
+  private_key_id: FAQ_KEY_ID,
+  private_key: FAQ_KEY,
   client_email: 'faqbot@rndiagbot.iam.gserviceaccount.com',
   client_id: '112398761636826928445',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
